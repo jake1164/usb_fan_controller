0a88bc431842c3bd9af8f0217d3b165ca9e35521
Jake1164 <jake1164@hotmail.com>
2020-05-15 12:07:22 -0400

    Renamed file to match directory. Added default credentials file.

---
+++ credentials.h
@@ -0,0 +1,20 @@
+  
+/*
+  If you clone this repository and want to keep your credentials out of github then
+  execute the following command to ignore any changes to this file and prevent being
+  promted to add this file when you update other files in github.
+  # git update-index --assume-unchanged credentials.h
+  To continue tracking changes:
+  # git update-index --no-assume-unchanged credentials.h
+*/
+#ifndef __CREDENTIALS_H__
+#define __CREDENTIALS_H__
+
+#define WIFI_SSID "Your SSID here"
+#define WIFI_PASSWD "Your WiFi Password Here"
+
+#define MQTT_ADDRESS "IP or Hostname goes here"
+#define MQTT_USERNAME "MQTT username goes here"
+#define MQTT_PASSWD "MQTT Password goes here"
+
+#endif
